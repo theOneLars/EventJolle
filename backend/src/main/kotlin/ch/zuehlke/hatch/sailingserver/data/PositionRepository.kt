2771aceb13f9b8f1121e68f657918089445e2990
Tobias Forster <tobias.forster@zuehlke.com>
2019-05-06 17:41:20 +0200

    Extend the PositionRepository with the filtering for time range.

--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/PositionRepository.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/PositionRepository.kt
@@ -2,22 +2,34 @@ package ch.zuehlke.hatch.sailingserver.data
 
 import ch.zuehlke.hatch.sailingserver.config.ApplicationConfig.Companion.COLLECTION_NAME_EVENTS
 import ch.zuehlke.hatch.sailingserver.domain.Position
-import com.mongodb.client.model.Filters.eq
+import com.mongodb.client.model.Filters.*
+import com.mongodb.client.model.Sorts
 import com.mongodb.reactivestreams.client.MongoDatabase
 import org.bson.Document
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Repository
 class PositionRepository @Autowired
 constructor(private val database: MongoDatabase) {
 
+    private val PROPERTY_TIMESTAMP = "updates.values.value.timestamp"
+
     fun getPositions(from: LocalDateTime, to: LocalDateTime): Flux<Position> {
+        val filter = and(
+                eq("updates.values.path", "navigation.position"),
+                gte(PROPERTY_TIMESTAMP, from.format(DateTimeFormatter.ISO_DATE_TIME)),
+                lt(PROPERTY_TIMESTAMP, to.format(DateTimeFormatter.ISO_DATE_TIME))
+        )
+
         return Flux.from(
-                this.database.getCollection(COLLECTION_NAME_EVENTS)
-                        .find(eq("updates.values.path", "navigation.position"))
+                this.database
+                        .getCollection(COLLECTION_NAME_EVENTS)
+                        .find(filter)
+                        .sort(Sorts.ascending(PROPERTY_TIMESTAMP))
         ).map { this.toPosition(it) }
     }
 
--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/sample/SampleController.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/sample/SampleController.kt
@@ -3,9 +3,11 @@ package ch.zuehlke.hatch.sailingserver.usecase.sample
 import ch.zuehlke.hatch.sailingserver.data.PositionRepository
 import ch.zuehlke.hatch.sailingserver.domain.Position
 import ch.zuehlke.hatch.sailingserver.signalk.SignalKService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -34,9 +36,15 @@ class SampleController {
                 .interval(Duration.ofMillis(2000))
                 .map { tick -> "string " + tick!! }
 
-    private val repoFlux: Flux<Position>
-        @GetMapping(path = ["/repoFlux"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-        get() = this.repository.getPositions(LocalDateTime.now(), LocalDateTime.now());
+    @GetMapping(path = ["/repoFlux"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun getPositions(@RequestParam("from")
+                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                     from: LocalDateTime,
+                     @RequestParam("to")
+                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                     to: LocalDateTime): Flux<Position> {
+        return this.repository.getPositions(from, to);
+    }
 
 }
 
