a182c13cd6d1939f6ffbd4640c414c2c9a31f9af
mminder <marvinminder@gmail.com>
2019-05-06 17:40:10 +0200

    implement smooth processing for wind speed

--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/cockpit/CockpitController.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/cockpit/CockpitController.kt
@@ -4,16 +4,12 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Flux
-
-import java.time.Duration
 
 @RestController
 @RequestMapping("/cockpit")
-class CockpitController {
-
+class CockpitController(val cockpitUseCase: CockpitUseCase) {
 
     @GetMapping(path = [""], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun getCockpit(): Flux<String> = Flux.interval(Duration.ofMillis(2000)).map { "cockpit data $it" }
+    fun getCockpit() = cockpitUseCase.getCockpit()
 }
 
---
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/cockpit/model/CockpitDto.kt
@@ -0,0 +1,5 @@
+package ch.zuehlke.hatch.sailingserver.usecase.cockpit.model
+
+import ch.zuehlke.hatch.sailingserver.processing.ApparentWind
+
+data class CockpitDto(val apparentWind: ApparentWind)
