d2a2ce596cf903dc2e6af93ef0a4c79a0d8a7360
Christian Ramseier <chrigi@gmail.com>
2019-05-09 11:07:42 +0200

    Make smoother a simple class

--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/SmoothedApparentWindRepository.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/SmoothedApparentWindRepository.kt
@@ -1,6 +1,5 @@
 package ch.zuehlke.hatch.sailingserver.data
 
-import ch.zuehlke.hatch.sailingserver.data.repository.ApparentWindRepository
 import ch.zuehlke.hatch.sailingserver.domain.ApparentWindMeasurement
 import ch.zuehlke.hatch.sailingserver.processing.ApparentWindSmoother
 import org.springframework.stereotype.Repository
