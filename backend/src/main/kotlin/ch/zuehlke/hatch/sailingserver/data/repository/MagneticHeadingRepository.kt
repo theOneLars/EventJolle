15c13937767b487859aea8391923d53d5367f54f
Christian Ramseier <chrigi@gmail.com>
2019-05-09 09:45:21 +0200

    Add repository for magnetic heading

--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/repository/MagneticHeadingRepository.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/repository/MagneticHeadingRepository.kt
@@ -1,27 +1,41 @@
 package ch.zuehlke.hatch.sailingserver.data.repository
 
+import ch.zuehlke.hatch.sailingserver.data.eventstore.EventStore
 import ch.zuehlke.hatch.sailingserver.domain.MagneticHeadingMeasurement
+import ch.zuehlke.hatch.sailingserver.domain.Position
 import ch.zuehlke.hatch.sailingserver.domain.Radiant
+import ch.zuehlke.hatch.sailingserver.livecache.LiveCache
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import java.time.Duration
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
+import javax.annotation.PostConstruct
 
 @Repository
-class MagneticHeadingRepository {
+class MagneticHeadingRepository(
+        private val eventStore: EventStore,
+        private val liveUpdateRepository: LiveUpdateRepository
+        ) {
 
-    fun getMockMagneticHeadingStream(): Flux<MagneticHeadingMeasurement> {
-        val startTime = Instant.now().epochSecond
-        return Flux
-                .interval(Duration.ofMillis(1000))
-                .map { tick ->
-                    val randomRadiant = Radiant(Math.random() * 2 * 3.14)
-                    val timestamp = LocalDateTime.ofEpochSecond(startTime + tick.toInt(), 0, ZoneOffset.UTC)
+    private val liveCache: LiveCache<MagneticHeadingMeasurement, TimeBasedIdentifier>
 
-                    MagneticHeadingMeasurement(randomRadiant, timestamp)
+    init {
+        val liveStream = this.liveUpdateRepository.getLiveStream(MagneticHeadingTransformer())
+        this.liveCache = LiveCache(liveStream) { heading -> TimeBasedIdentifier(heading.timestamp) }
+    }
 
-                }
+    fun getMagneticHeading(): Flux<MagneticHeadingMeasurement> {
+        return this.liveUpdateRepository.getLiveStream(MagneticHeadingTransformer())
+    }
+
+    fun getMagneticHeading(from: LocalDateTime): Flux<MagneticHeadingMeasurement> {
+        val find = this.eventStore.find(from, MagneticHeadingTransformer())
+        return this.liveCache.withSnapshot(find)
+    }
+
+    fun getHistoryMagneticHeading(from: LocalDateTime, to: LocalDateTime): Flux<MagneticHeadingMeasurement> {
+        return this.eventStore.find(from, to, MagneticHeadingTransformer())
     }
 }
---
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/repository/MagneticHeadingTransformer.kt
@@ -0,0 +1,24 @@
+package ch.zuehlke.hatch.sailingserver.data.repository
+
+import ch.zuehlke.hatch.sailingserver.data.eventstore.DocumentValueExtractor
+import ch.zuehlke.hatch.sailingserver.data.eventstore.EventTransformer
+import ch.zuehlke.hatch.sailingserver.domain.MagneticHeadingMeasurement
+import ch.zuehlke.hatch.sailingserver.domain.Position
+import ch.zuehlke.hatch.sailingserver.domain.Radiant
+import org.bson.Document
+
+class MagneticHeadingTransformer : EventTransformer<MagneticHeadingMeasurement> {
+
+    override fun transform(document: Document): List<MagneticHeadingMeasurement> {
+        val extractor = DocumentValueExtractor.from(document, getPath())
+        return extractor.extract { time, document ->
+            val double = document.getDouble("value")
+            MagneticHeadingMeasurement(Radiant(double), time)
+        }
+    }
+
+    override fun getPath(): String {
+       return "navigation.headingMagnetic"
+    }
+
+}
