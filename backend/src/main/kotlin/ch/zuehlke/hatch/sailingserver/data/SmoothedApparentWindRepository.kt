f0b4aa5489c6b2a6c94b94012f19fe9f72cef7d4
mminder <marvinminder@gmail.com>
2019-05-09 17:05:26 +0200

    implemented error state aware map function

--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/SmoothedApparentWindRepository.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/data/SmoothedApparentWindRepository.kt
@@ -12,14 +12,8 @@ class SmoothedApparentWindRepository(private val apparentWindRepository: Apparen
     fun getSmoothApparentWindStream(): Flux<MeasurementMessage<ApparentWindMeasurement>> {
         val smoother = ApparentWindSmoother()
         return apparentWindRepository.getApparentWindStream()
-                .map { message ->
-                    when (message) {
-                        is MeasurementMessage.Data -> message.measurement
-                        else -> throw RuntimeException("error handling not yet implemented")
-                    }
-                }
-                .map { smoother.smooth(it) }
-                .map { MeasurementMessage.Data(it) }
+                .map { mapOrPropagateError(it, smoother::smooth) }
     }
+
 
 }
