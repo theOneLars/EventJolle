706e949aa636adae2100c596444ad35154cbff4d
Tobias Forster <tobias.forster@zuehlke.com>
2019-07-10 10:55:52 +0200

    Introduce a unit test to verify if the CourseOverGroundTransformer is now able to handle integer values, which caused a ClassCastException in the previous implementation using the "org.bson.Document".

---
+++ backend/src/test/kotlin/ch/zuehlke/hatch/sailingserver/data/repository/CourseOverGroundTransformerTest.kt
@@ -0,0 +1,50 @@
+package ch.zuehlke.hatch.sailingserver.data.repository
+
+import ch.zuehlke.hatch.sailingserver.data.eventstore.EventTransformer
+import ch.zuehlke.hatch.sailingserver.domain.CourseOverGroundMeasurement
+import ch.zuehlke.hatch.sailingserver.domain.Radiant
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+internal class CourseOverGroundTransformerTest {
+
+    private var transformer: EventTransformer<CourseOverGroundMeasurement> = CourseOverGroundTransformer()
+
+    @Test
+    fun testTransformWithValidCourseOverGround() {
+        val jsonObject = givenJsonWithValidCourseOverGround()
+
+        val actual = this.transformer.transform(jsonObject)
+
+        assertThat(actual)
+                .containsExactly(
+                        CourseOverGroundMeasurement(Radiant(5.625021646252525), LocalDateTime.of(2017, 8, 12, 4, 19, 30)))
+    }
+
+    @Test
+    fun testTransformWithZeroCourseOverGround() {
+        val jsonObject = givenJsonWithZeroCourseOverGround()
+
+        val actual = this.transformer.transform(jsonObject)
+
+        assertThat(actual)
+                .containsExactly(
+                        CourseOverGroundMeasurement(Radiant(0.0), LocalDateTime.of(2017, 8, 12, 4, 19, 30)))
+    }
+
+    private fun givenJsonObject(json: String): JsonObject {
+        val parser = JsonParser()
+        return parser.parse(json).asJsonObject
+    }
+
+    private fun givenJsonWithValidCourseOverGround(): JsonObject {
+        return givenJsonObject("{\"updates\":[{\"timestamp\":\"2017-08-12T04:19:30.000Z\",\"values\":[{\"path\":\"navigation.courseOverGroundTrue\",\"value\":5.625021646252525}]}]}")
+    }
+
+    private fun givenJsonWithZeroCourseOverGround(): JsonObject {
+        return givenJsonObject("{\"updates\":[{\"timestamp\":\"2017-08-12T04:19:30.000Z\",\"values\":[{\"path\":\"navigation.courseOverGroundTrue\",\"value\":0}]}]}")
+    }
+}
