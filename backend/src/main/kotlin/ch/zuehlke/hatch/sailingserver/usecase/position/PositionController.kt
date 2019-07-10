723449d55feb859e3741eb113b3a0dd4bc86e26a
Tobias Forster <tobias.forster@zuehlke.com>
2019-07-10 14:10:33 +0200

    Introduce further REST endpoints to load the positions for today and by a specific date.

--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/position/PositionController.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/position/PositionController.kt
@@ -1,15 +1,28 @@
 package ch.zuehlke.hatch.sailingserver.usecase.position
 
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/position")
-class PositionController(val positionUseCase: PositionUseCase) {
+class PositionController(
+        val positionUseCase: PositionUseCase
+) {
 
     @GetMapping(path = ["10years"])
     fun getLast10Years() = positionUseCase.getPositionsOfLast10Years()
 
+    @GetMapping(path = ["today"])
+    fun getToday() = positionUseCase.getPositionsOfToday()
+
+    @GetMapping(path = ["byDate"])
+    fun getForDate(@RequestParam("date")
+                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                 date: LocalDate) = positionUseCase.getPositionsOfDate(date)
 }
 
--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/position/PositionUseCase.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/usecase/position/PositionUseCase.kt
@@ -4,6 +4,7 @@ import ch.zuehlke.hatch.sailingserver.data.CompressedPositionRepository
 import ch.zuehlke.hatch.sailingserver.usecase.position.model.PositionDto
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Service
@@ -12,8 +13,20 @@ class PositionUseCase(val positionRepository: CompressedPositionRepository) {
     fun getPositionsOfLast10Years(): Flux<PositionDto> {
         val now = LocalDateTime.now()
         val tenYearsAgo = now.minusYears(10)
+
         return this.positionRepository.getCompressedPositionStream(tenYearsAgo, now)
                 .map { PositionDto(it.timestamp, it.longitude, it.latitude) }
     }
 
+    fun getPositionsOfToday(): Flux<PositionDto> {
+        return getPositionsOfDate(LocalDate.now())
+    }
+
+    fun getPositionsOfDate(date: LocalDate): Flux<PositionDto> {
+        val from = date.atStartOfDay()
+        val to = date.atTime(23, 59, 59, 999)
+
+        return this.positionRepository.getCompressedPositionStream(from, to)
+                .map { PositionDto(it.timestamp, it.longitude, it.latitude) }
+    }
 }
