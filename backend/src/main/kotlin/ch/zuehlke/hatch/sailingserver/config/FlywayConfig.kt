abb3e75f5e8d3cb8ace40808b712ee5b65b8593d
Marius Reusch <marius.reusch@gmail.com>
2019-07-10 14:35:59 +0200

    configure flyway migration during spring boot startup

--- backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/config/FlywayConfig.kt
+++ backend/src/main/kotlin/ch/zuehlke/hatch/sailingserver/config/FlywayConfig.kt
@@ -1,2 +1,21 @@
 package ch.zuehlke.hatch.sailingserver.config
 
+import org.flywaydb.core.Flyway
+import org.springframework.context.annotation.Configuration
+import javax.annotation.PostConstruct
+
+@Configuration
+class FlywayConfig(private val databaseConfigProperties: DatabaseConfigProperties) {
+
+
+    @PostConstruct
+    fun migrateFlyway() {
+
+        val flyway = Flyway.configure()
+                .dataSource("jdbc:postgresql://${databaseConfigProperties.host}:${databaseConfigProperties.port}/${databaseConfigProperties.databaseName}",
+                        databaseConfigProperties.username,
+                        databaseConfigProperties.password).load()
+        flyway.migrate()
+
+    }
+}
